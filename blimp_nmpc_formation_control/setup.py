1b2ec50840f00c01143e4706bf86f1f1c101a6a6
Mohamed Taha <mohamed.a.m.i.taha@gmail.com>
2025-06-17 15:43:39 +0200

    Add formation configuration, package definition, and ignored original ROS1 package
    - Created formation_config.py to define vehicle parameters and constraints for the blimp formation control.
    - Added package.xml to define package dependencies and metadata for the blimp_nmpc_formation_control.
    - Included ROS1 package for migration purposed with COLCON_IGNORE

---
+++ blimp_nmpc_formation_control/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'blimp_nmpc_formation_control'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Mohamed-Taha',
+    maintainer_email='mohamed.a.m.i.taha@gmail.com',
+    description='Calculate and relay formation parameters to blimps',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'blimp_nmpc_formation_controller = blimp_nmpc_formation_control.blimp_nmpc_formation_controller:main'
+        ],
+    },
+)
