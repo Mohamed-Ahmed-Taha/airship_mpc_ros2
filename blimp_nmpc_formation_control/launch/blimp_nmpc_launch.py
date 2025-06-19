0521694e997eb98171cf5df59d2e9c1e73189ce0
Mohamed Taha <mohamed.a.m.i.taha@gmail.com>
2025-06-19 20:45:54 +0200

    Refactor launch file to streamline node creation and improve argument handling

--- blimp_nmpc_formation_control/launch/blimp_nmpc_launch.py
+++ blimp_nmpc_formation_control/launch/blimp_nmpc_launch.py
@@ -1,28 +1,17 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, OpaqueFunction
 from launch.substitutions import LaunchConfiguration
 
-def generate_launch_description():
-    """generate the launch description"""
-    # Declare launch argument for number of machines
-    num_machines_arg = DeclareLaunchArgument(
-        'num_machines',
-        default_value='3',
-        description='Number of blimps to launch (integer)'
-    )
-
-    # Initialize LaunchDescription
-    ld = LaunchDescription()
-
-    # Add the launch argument
-    ld.add_action(num_machines_arg)
-
-    # Get the number of machines from launch argument
-    num_machines = LaunchConfiguration('num_machines')
-
+def launch_nodes(context, num_machines):
+    # Evaluate the num_machines launch argument
+    num = int(context.perform_substitution(num_machines))
+    
+    # Create a list to hold node actions
+    nodes = []
+    
     # Loop to create nodes for each blimp
-    for robot_id in range(1, int(num_machines) + 1):
+    for robot_id in range(1, num + 1):
         node = Node(
             package='blimp_nmpc_formation_control',
             executable='blimp_nmpc_formation_controller',
@@ -30,7 +19,7 @@ def generate_launch_description():
             namespace=f'machine_{robot_id}',
             parameters=[{
                 'robotID': robot_id,
-                'numRobots': int(num_machines),
+                'numRobots': num,
                 'PREFIX': '/machine_',
                 # 'POSETOPIC': 'throttledUAVPose',
                 'use_sim_time': False,
@@ -38,6 +27,25 @@ def generate_launch_description():
             }],
             output='screen'
         )
-        ld.add_action(node)
+        nodes.append(node)
+    
+    return nodes
 
+def generate_launch_description():
+    # Declare launch argument for number of machines
+    num_machines_arg = DeclareLaunchArgument(
+        'num_machines',
+        default_value='3',
+        description='Number of blimps to launch (integer)'
+    )
+    
+    # Get the number of machines launch configuration
+    num_machines = LaunchConfiguration('num_machines')
+    
+    # Create LaunchDescription
+    ld = LaunchDescription([
+        num_machines_arg,
+        OpaqueFunction(function=launch_nodes, args=[num_machines])
+    ])
+    
     return ld
